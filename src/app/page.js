22f30e1d7d209bef68ecaf4c6db36eacb6483a2d
yasminmo2021 <y.moreno@estudante.ifro.edu.br>
2025-02-20 09:53:39 -0400

    acaboouuuuuuu

--- src/app/page.js
+++ src/app/page.js
@@ -19,7 +19,7 @@ export default function Home() {
   return (
     <main>
          <h1 className={styles.h1}>Seja bem-vindo!❤️</h1>
-        <p className={styles.text}>Bem-vindo à Clínica Dom da Vida, onde a sua saúde e bem-estar são nossa maior prioridade. Estamos prontos para oferecer um atendimento completo e especializado com uma equipe multidisciplinar altamente qualificada, pronta para atender às suas necessidades de forma acolhedora e humana. Nosso ambiente moderno foi pensado para proporcionar conforto e tranquilidade, oferecendo a segurança que você precisa para cuidar de sua saúde com confiança. Seja para consultas preventivas ou tratamentos especializados, estamos aqui para cuidar de você de forma integral. Venha nos visitar e experimente o atendimento diferenciado que preza pelo seu bem-estar em cada detalhe.</p>
+        <p className={styles.text}>Bem-vindo à Clínica Dom da Vida, onde a sua saúde e bem-estar são nossa maior prioridade. Estamos prontos para oferecer um atendimento completo e especializado com uma equipe multidisciplinar altamente qualificada, pronta para atender às suas necessidades de forma acolhedora e humana. Nosso ambiente moderno foi pensado para proporcionar conforto e tranquilidade, oferecendo a segurança que você precisa para cuidar de sua saúde com confiança. Seja para consultas preventivas ou tratamentos especializados, estamos aqui para cuidar de você de forma integral.</p>
          <button className={styles.buttonMedic}>Agende sua consulta</button>
         
   
